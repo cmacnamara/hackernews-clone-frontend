68d3aa18450e0d68bb393e85d2a77cee2323c700
Chris MacNamara <cpmacnamara@gmail.com>
2023-11-08 11:31:07 -0500

    add link and linklist components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
 import './../styles/App.css'
+import LinkList from './LinkList'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-
+      <LinkList />
     </>
   )
 }
