8e3fb4fe1357b746cf1dd71122cfed65bc73a8a4
Chris MacNamara <cpmacnamara@gmail.com>
2023-11-08 15:28:07 -0500

    add authToken

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import '../styles/index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { setContext } from '@apollo/client/link/context'
+import { AUTH_TOKEN } from '../constants.js'
 
 import {
   ApolloProvider,
@@ -15,8 +17,18 @@ const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
